6b5e7fa2d36872b2553239b12138f7acf191595a
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-06-03 19:17:22 +0100

    drag and drop for the columns

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,15 +5,35 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 function Board() {
-  const [getBoard, board] = useBoardStore((state) => [
+  const [getBoard, board, setBoardState] = useBoardStore((state) => [
     state.getBoard,
     state.board,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    // Check if user dragged card outside of board
+    if (!destination) return;
+
+    //Handle column drag
+    if (type === 'column') {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+    // This step is needed as the indexes are stored as numbers 0,1,2 etc. instead of id's with DND library
+    const columns = Array.from(board.columns);
+  };
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId='board' direction='horizontal' type='column'>
