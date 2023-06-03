0dbf30224305f9ddb67361af0e9d96e7ebe1fca9
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-06-03 19:39:57 +0100

    persist drag and drop for cards

--- components/Board.tsx
+++ components/Board.tsx
@@ -31,8 +31,68 @@ function Board() {
         columns: rearrangedColumns,
       });
     }
-    // This step is needed as the indexes are stored as numbers 0,1,2 etc. instead of id's with DND library
-    const columns = Array.from(board.columns);
+
+    if (type === 'card') {
+      // This step is needed as the indexes are stored as numbers 0,1,2 etc. instead of id's with DND library
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const finishColIndex = columns[Number(destination.droppableId)];
+
+      const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+      };
+
+      const finishCol: Column = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+      };
+
+      // Check if user dragged card to same column or same position
+      if (!startCol || !finishCol) return;
+      if (source.index === destination.index && startCol === finishCol) return;
+
+      const newTodos = startCol.todos;
+      const [todoMoved] = newTodos.splice(source.index, 1);
+
+      if (startCol.id === finishCol.id) {
+        //Same column task drag
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol: Column = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+
+        const newColumns = new Map(board.columns);
+        newColumns.set(newCol.id, newCol);
+
+        setBoardState({
+          ...board,
+          columns: newColumns,
+        });
+      } else {
+        // dragging to another column
+        const finishTodos = Array.from(finishCol.todos);
+        finishTodos.splice(destination.index, 0, todoMoved);
+
+        const newColumns = new Map(board.columns);
+        const newCol: Column = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+
+        newColumns.set(startCol.id, newCol);
+        newColumns.set(finishCol.id, {
+          id: finishCol.id,
+          todos: finishTodos,
+        });
+
+        setBoardState({
+          ...board,
+          columns: newColumns,
+        });
+      }
+    }
   };
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
